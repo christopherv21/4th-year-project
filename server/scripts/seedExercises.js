db8e6e273c5cfedd695080a53366416c66096e98
christopherv21 <B00146996@mytudublin.ie>
2026-04-18 18:22:11 +0100

    Update seedExercises.js

--- server/scripts/seedExercises.js
+++ server/scripts/seedExercises.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config({ path: __dirname + "/../.env" });
 
 const connectDB = require("../config/db");
 const Exercise = require("../models/Exercise");
