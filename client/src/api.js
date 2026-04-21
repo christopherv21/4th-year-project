338d4807a2f612456d98c5498d633ad089002751
christopherv21 <B00146996@mytudublin.ie>
2026-04-21 20:10:37 +0100

    Use production backend URL

--- client/src/api.js
+++ client/src/api.js
@@ -1,5 +1,4 @@
-const API_BASE =
-  process.env.REACT_APP_API_URL || "http://localhost:5000";
+const API_BASE = "https://fourth-year-project-8fyo.onrender.com";
 
 export async function apiFetch(path, options = {}) {
   const token = localStorage.getItem("token");
